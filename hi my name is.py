412c4f47d4023d599394b8f7c7217b50c1f8ad08
logan-ebensperger <loganebens@gmail.com>
2025-10-24 13:09:01 -0500

    input print

---
+++ hi my name is.py
@@ -0,0 +1,7 @@
+name = input ("Enter your name")
+age = int(input("Enter your age"))
+hobby = input ("Enter your favorite hobby: ")
+
+print ("Hello my name is" + name + "my age is" + str(age) + "and my hobby is " + hobby)
+
+print(f" Hello my name is {name}, my age is {age} and my hobby is {hobby}.")
