33de673461a22400d9603690f2834c0ff3bb2098
logan-ebensperger <loganebens@gmail.com>
2025-10-28 12:53:33 -0500

    averges

---
+++ # Prompt the user to enter the first num.py
@@ -0,0 +1,11 @@
+num1 = float(input("Enter the first number: "))
+
+num2 = float(input("Enter the second number: "))
+
+num3 = float(input("Enter the third number: "))
+
+total_sum = num1 + num2 + num3
+
+average = total_sum / 3
+
+print("The average of the three numbers is:", average)
