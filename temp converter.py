453cc26602e0ab155224ea4b5aec4857dd530752
logan-ebensperger <loganebens@gmail.com>
2025-10-30 12:53:23 -0500

    time converter

--- temp converter.py
+++
@@ -1,6 +0,0 @@
-fahrenheit = float(input("Enter the temperature in Farenhite(°F): "))
-
-celsius = (fahrenheit - 32) * 5/9
-kelvin = celsius +273.15
-
-print (f"{fahrenheit:.1f}°F is equal to {celsius:.2f} °C and {kelvin:.2f} K")
---
+++ time converter.py
@@ -0,0 +1,7 @@
+hours = float(input("Enter the number of hours: "))
+
+minutes = hours * 60
+seconds = hours * 3600
+
+print(f"{hours} hours is eqal to {minutes} minutes.")
+print(f"{hours} hours is equal to {seconds} seconds. ")
