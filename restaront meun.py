af87f498a23317345184f2aaeb0a2a6a3878bc3e
logan-ebensperger <loganebens@gmail.com>
2025-11-04 12:52:48 -0600

    restaront meun

---
+++ restaront meun.py
@@ -0,0 +1,16 @@
+print("Welcome to Our Restaurant Menu\n")
+
+print("Appetizers:")
+print("1. Garlic Bread 🧈: $5.99")
+print("2. Bruschetta 🍕: $6.99")
+print("3. Mozzarella Sticks 🧀: $7.99\n")
+
+print("Main Courses:")
+print("1. Spaghetti Bolognese 🍝: $12.99")
+print("2. Chicken Alfredo 🍗: $14.99")
+print("3. Steak with Mashed Potatoes 🥩: $19.99\n")
+
+print("Desserts:")
+print("1. Tiramisu 🍰: $6.99")
+print("2. Chocolate Cake 🍫: $5.99")
+print("3. Fruit Salad 🍓🍌: $4.99")
